307d75e0d59cb9f8d8e2471f591c929e884d408e
Tushar Meshram <109428259+meshramtushar52@users.noreply.github.com>
2022-07-25 23:50:22 +0530

    Add files via upload

---
+++ Arrays/InitializeAllValues.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[100];
+
+    fill_n(arr , 100 , 7);
+
+    for(int i = 0; i < 100; i++){
+
+        cout << arr[i] << " " ;
+    }
+    return 0;
+}
