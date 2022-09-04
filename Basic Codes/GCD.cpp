7f7ffb76b1c81844610e88ea838ba3eae229d38c
Tushar Meshram <109428259+meshramtushar52@users.noreply.github.com>
2022-09-04 10:56:48 +0530

    Create GCD.cpp

---
+++ Basic Codes/GCD.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a , int b)
+{
+    if (a == 0){
+        return b;
+    }
+    if ( b == 0){
+        return a;
+    }
+    if ( a == b){
+        return a;
+    }
+    if (a > b){
+        return gcd( a-b ,b);
+    }
+    return gcd( a, b-a);
+}
+
+int main()
+{
+    int a, b;
+    cout << " Enter two numbers ";
+    cin >> a>> b;
+    cout << " GCD of two numbers ";
+    cout << gcd(a,b) << endl;
+
+    return 0;
+}
